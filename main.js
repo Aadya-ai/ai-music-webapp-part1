5c722fee4ca890dc005384d1dd8ca33f856bbd79
Candy Crush <76581343+Aadya-ai@users.noreply.github.com>
2021-07-31 12:49:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,100 @@
+song_1 = "";
+song_2 = "";
+
+function preload() {
+    song_1 = loadSound("OFFICIAL MUSIC VIDEO _ BABY DON’T WANT YOU _ Baby Queen _ Rimorav Vlogs presents RI Vlogs [YTmp3.net].mp3");
+    song_2 = loadSound("OFFICIAL MUSIC VIDEO _ VAARI JAWAN _ Rimorav Vlogs [YTmp3.net].mp3");
+}
+
+function setup() {
+    canvas = createCanvas(550, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+song1_status = "";
+song2_status = "";
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+	console.log(results);
+	scoreRightWrist =  results[0].pose.keypoints[10].score;
+	scoreLeftWrist =  results[0].pose.keypoints[9].score;
+	console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+	
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+		
+  }
+}
+
+function draw() {
+	image(video, 0, 0, 600, 500);
+	
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
+
+	fill("#00008B");
+	stroke("#00008B");
+
+	if(scoreRightWrist > 0.2)
+	{ 
+		circle(rightWristX,rightWristY,20);
+
+			song2.stop();
+
+		if(song1_status == false)
+		{
+			song1.play();
+		}
+	}
+
+	if(scoreLeftWrist > 0.2)
+	{
+		circle(leftWristX,leftWristY,20);
+
+			song1.stop();
+
+		if(song2_status == false)
+		{
+			song2.play();
+		}
+	}
+
+}
+
+function play()
+{
+	song_1.play();
+	song_1.setVolume(1);
+	song_1.rate(1);
+}
+
+function stop() {
+    song_1.stop();
+}
